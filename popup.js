8c36a71faed0ba08d73643044a9566a4405d298e
Olfy20 <olfy20@mail.ru>
2018-11-19 08:53:21 +0300

    bug fix, added console logging

--- popup.js
+++ popup.js
@@ -9,8 +9,18 @@ document.addEventListener('DOMContentLoaded', function() {
         chrome.tabs.getAllInWindow(null, function(tabs){
             for (var i = 0; i < tabs.length; i++) 
                 new_group[i] = tabs[i].url;
-
-            chrome.storage.local.set({new_group_name, new_group});
+                
+        var value = new_group;
+        var key = new_group_name;
+        
+        chrome.storage.local.set({key: value}, function() {
+          console.log('Value is set to ' + value);
+        });
+      
+        chrome.storage.local.get(['key'], function(result) {
+          console.log('Value currently is ' + result.key);
+        });
+        
         });        
 	},false);
 },false);
