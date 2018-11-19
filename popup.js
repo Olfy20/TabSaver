0e3e041ea668a989754bea505665aaa4089268a1
Olfy20 <olfy20@mail.ru>
2018-11-19 10:28:02 +0300

    bug fix: incorrect keys

--- popup.js
+++ popup.js
@@ -1,26 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     var newGroupButton = document.getElementById('new_group');
 	newGroupButton.addEventListener('click', function() {
-		var new_group_name = document.getElementById('new_group_name').value;
-		if(new_group_name == '')
-			new_group_name = new Date().toLocaleString();
-		var new_group = [];
+		var key = document.getElementById('new_group_name').value == '' ? new Date().toLocaleString() : document.getElementById('new_group_name').value;
+		var value = [];
         
         chrome.tabs.getAllInWindow(null, function(tabs){
             for (var i = 0; i < tabs.length; i++) 
-                new_group[i] = tabs[i].url;
-                
-        var value = new_group;
-        var key = new_group_name;
-        
-        chrome.storage.local.set({key: value}, function() {
-          console.log('Value is set to ' + value);
-        });
-      
-        chrome.storage.local.get(['key'], function(result) {
-          console.log('Value currently is ' + result.key);
-        });
-        
+                value.push(tabs[i].url);
+            
+            console.log('key:'+key);
+            var new_group = {};
+            new_group[key] = value;
+
+            chrome.storage.local.set(new_group);
+            console.log(new_group);        
         });        
 	},false);
 },false);
