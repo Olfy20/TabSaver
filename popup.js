5fb9e6783be3881916fb841a379ad8308120e081
Olfy20 <olfy20@mail.ru>
2018-11-20 04:33:43 +0300

    added docBlock comments

--- popup.js
+++ popup.js
@@ -1,13 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var newGroupButton = document.getElementById('new_group');
-    newGroupButton.addEventListener('click', function() {
+    /**
+    * Добавление новой группы вкладок
+    */
+    document.getElementById('new_group').addEventListener('click', function() {
+        /**
+        * Id группы вкладок
+        *
+        * @var string $key
+        */
         var key = document.getElementById('new_group_name').value == '' ? new Date().toLocaleString() : document.getElementById('new_group_name').value;
+        /**
+        * URL адреса группы вкладок
+        *
+        * @var object $value
+        */
 		var value = [];
         chrome.tabs.getAllInWindow(null, function(tabs){
             for (var i = 0; i < tabs.length; i++) 
                 value.push(tabs[i].url);
             
         console.log('key:'+key);
+        /**
+        * Группа вкладок
+        *
+        * @var object $new_group
+        */
         var new_group = {};
         new_group[key] = value;
         chrome.storage.local.set(new_group);
@@ -17,26 +34,48 @@ document.addEventListener('DOMContentLoaded', function() {
     },false);
 },false);
 
+/**
+ *Удаляет текущую группу вкладок из хранилища
+ */
 function deleteGroup(){
     console.log('test del'+this.id.slice(1));
     chrome.storage.local.remove(this.id.slice(1));
     reload();
 }
 
+/**
+ *Открывает текущую группу вкладок в новом окне
+ */
 function openGroup(){
     console.log('test open'+this.id.slice(1));
-    var profile = this.id.slice(1);
-    chrome.storage.local.get(profile, function(item){    
-    tabURLs = [];
-     chrome.windows.create({url:item[profile]}, function(window){
+    /**
+        * Id группы вкладок
+        *
+        * @var string $key
+    */
+    var key = this.id.slice(1);
+    chrome.storage.local.get(key, function(item){    
+        chrome.windows.create({url:item[key]}, function(window){
     });
    });
 }
 
+/**
+ *Обновляет страницу расширения
+ */
 function reload(){   
     chrome.storage.local.get(null, function(items) {
-        var data = document.getElementById('data');
+        /**
+        * Id группы вкладок
+        *
+        * @var string $key
+        */
         var keys = Object.keys(items);
+        /**
+        * html-код для отображения сохраненных групп 
+        *
+        * @var string $html
+        */
         var html = '';
         for (var i = 0; i < keys.length; i++){      
             html += '<div class="group">';
@@ -45,7 +84,7 @@ function reload(){
             html += '</div>';
         }
         console.log('why:' + html);
-        data.innerHTML = html;
+        document.getElementById('data').innerHTML = html;
         
         for (var i = 0; i < keys.length; i++){                            
             document.getElementById('o'+keys[i]).addEventListener("click", openGroup);
